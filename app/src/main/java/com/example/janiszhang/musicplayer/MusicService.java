439b9ceac83ee0e01230785acc89f3aa59e62ffc
zhangbz <zhangbz.happyhacking@gmail.com>
2016-03-25 03:01:42 +0800

    version3:add notification

--- app/src/main/java/com/example/janiszhang/musicplayer/MusicService.java
+++ app/src/main/java/com/example/janiszhang/musicplayer/MusicService.java
@@ -1,6 +1,10 @@
 package com.example.janiszhang.musicplayer;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Binder;
@@ -9,7 +13,10 @@ import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.text.TextUtils;
 import android.util.Log;
+import android.widget.RemoteViews;
 
 import java.util.ArrayList;
 
@@ -23,10 +30,18 @@ public class MusicService extends Service{
     private ArrayList<MusicData> mMusicDatas;
     private int i = 0;
     private MediaPlayer mMediaPlayer;
+    public static final String ACTION_NOTIFICATION = "action_notification";
+    public static final String BUTTON_INDEX = "button_index";
+    public static final String BUTTON_PREV = "0";
+    public static final String BUTTON_PLAY = "1";
+    public static final String BUTTON_NEXT = "2";
 
     private boolean isPlaying = false;
 
     Messenger mMessenger = new Messenger(new IncomingHandler());
+    private RemoteViews mRemoteViews;
+    private NotificationCompat.Builder mBuilder;
+    private NotificationManager mNotificationManager;
 
     class IncomingHandler extends Handler {
         @Override
@@ -65,6 +80,7 @@ public class MusicService extends Service{
     @Override
     public void onCreate() {
         super.onCreate();
+
         mMusicList = MusicList.getMusicList();
         mMusicDatas = mMusicList.getList();
         mMediaPlayer = MediaPlayer.create(this, mMusicDatas.get(i).getSrc());
@@ -75,13 +91,78 @@ public class MusicService extends Service{
                 mMyBinder.next();
             }
         });
+
         Log.i("zhangbz", "service onCreate");
+
+        mRemoteViews = new RemoteViews(getPackageName(), R.layout.music_notification);
+        mRemoteViews.setTextViewText(R.id.music_name, mMusicDatas.get(i).getName());
+        mRemoteViews.setTextViewText(R.id.singer_name, mMusicDatas.get(i).getSinger());
+
+        Intent intent = new Intent(this, MusicService.class);
+        intent.setAction(ACTION_NOTIFICATION);
+
+        intent.putExtra(BUTTON_INDEX, BUTTON_PREV);
+        PendingIntent pendingIntentPrev = PendingIntent.getService(this, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        mRemoteViews.setOnClickPendingIntent(R.id.btn_prev, pendingIntentPrev);
+
+        intent.putExtra(BUTTON_INDEX, BUTTON_PLAY);
+        pendingIntentPrev = PendingIntent.getService(this, 2, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        mRemoteViews.setOnClickPendingIntent(R.id.btn_play, pendingIntentPrev);
+
+        intent.putExtra(BUTTON_INDEX, BUTTON_NEXT);
+        pendingIntentPrev = PendingIntent.getService(this, 3, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        mRemoteViews.setOnClickPendingIntent(R.id.btn_next, pendingIntentPrev);
+
+        mBuilder = new NotificationCompat.Builder(this);
+        mBuilder.setSmallIcon(R.drawable.default_pic);
+        mBuilder.setContent(mRemoteViews);
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(233, mBuilder.build());
 
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.i("zhangbz", "service onStartCommand");
+
+        String action = intent.getAction();
+        String stringExtra = intent.getStringExtra(BUTTON_INDEX);
+        if(TextUtils.equals(action, ACTION_NOTIFICATION)) {
+            if (TextUtils.equals(stringExtra, BUTTON_PREV)) {
+                i = (i-1)<0?mMusicDatas.size()-1:i-1;
+                mMediaPlayer.stop();
+                mMediaPlayer = MediaPlayer.create(MusicService.this, mMusicDatas.get(i).getSrc());
+                if(isPlaying) {
+                    mMediaPlayer.start();
+                }
+                mRemoteViews.setTextViewText(R.id.music_name, mMusicDatas.get(i).getName());
+                mRemoteViews.setTextViewText(R.id.singer_name, mMusicDatas.get(i).getSinger());
+                mNotificationManager.notify(233, mBuilder.build());
+            } else if(TextUtils.equals(stringExtra, BUTTON_PLAY)) {
+                if(mMediaPlayer.isPlaying()){
+                    isPlaying =false;
+                    mMediaPlayer.pause();
+                    mRemoteViews.setImageViewResource(R.id.btn_play, R.drawable.note_btn_play);
+                    mNotificationManager.notify(233, mBuilder.build());
+                } else {
+                    isPlaying = true;
+                    mMediaPlayer.start();
+                    mRemoteViews.setImageViewResource(R.id.btn_play,R.drawable.note_btn_pause);
+                    mNotificationManager.notify(233, mBuilder.build());
+                }
+
+            } else {
+                i = (i+1)>=mMusicDatas.size()? 0 : i+1;
+                mMediaPlayer.stop();
+                mMediaPlayer = MediaPlayer.create(MusicService.this, mMusicDatas.get(i).getSrc());
+                if(isPlaying) {
+                    mMediaPlayer.start();
+                }
+                mRemoteViews.setTextViewText(R.id.music_name, mMusicDatas.get(i).getName());
+                mRemoteViews.setTextViewText(R.id.singer_name, mMusicDatas.get(i).getSinger());
+                mNotificationManager.notify(233, mBuilder.build());
+            }
+        }
         return super.onStartCommand(intent, flags, startId);
     }
 
